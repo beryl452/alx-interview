cd999dd4e993605c613d64cb95814cf4a2b83b7f
beryl452 <berylhoe+alx@gmail.com>
2024-03-11 09:39:51 +0000

    End of 0-lockboxes.py

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+0-lockboxes.py
+"""
+
+def canUnlockAll(boxes):
+    """
+    Determines if all the boxes can be opened.
+    Return True if all boxes can be opened, else return False
+    """
+    for i in range(0, len(boxes)):
+        if ((i != len(boxes) - 1) and not boxes[i]):
+            return False
+    return True
