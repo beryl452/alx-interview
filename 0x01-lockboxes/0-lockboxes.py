25d9911848f4ca8c06ce184f2cb646c243f84b42
beryl452 <berylhoe+alx@gmail.com>
2024-03-13 10:58:03 +0000

    Fix 0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -9,7 +9,17 @@ def canUnlockAll(boxes):
     Determines if all the boxes can be opened.
     Return True if all boxes can be opened, else return False
     """
-    for i in range(0, len(boxes)):
-        if ((i != len(boxes) - 1) and not boxes[i]):
-            return False
-    return True
+    boxeLength = len(boxes)
+    if boxeLength == 0:
+        return True
+    unlocked = [False] * boxeLength
+    unlocked[0] = True
+    keys = boxes[0]
+
+    while keys:
+        key = keys.pop()
+        if key < boxeLength and not unlocked[key]:
+            unlocked[key] = True
+            keys += boxes[key]
+
+    return all(unlocked)
